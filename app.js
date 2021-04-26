a6621927cfc99ea42095bab375ec504a1361b18d
MRyzhylo <playa77777@gmail.com>
2021-04-26 13:21:17 +0300

    create mysql database connection

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const config = require('config');
+const mysql = require('mysql');
+
+const app = express()
+
+const db = mysql.createConnection({
+    host: config.get('host'),
+    user: config.get('user'),
+    password: config.get('pass'),
+    database: config.get('db_name')
+});
+
+db.connect( (err)=> {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Database conected...')
+    }
+});
+
+const PORT = config.get('port')
+
+app.listen(PORT, ()=> console.log(`App has been started on port: ${PORT}`))
